04f12b8db803e60d9a93949102c10b411dab20c5
Gonzalo Carreno <kronleuchter85@gmail.com>
2023-04-24 18:30:33 +0100

    agregando prueba de concepto con sensor BMP280

---
+++ pocs/esp32-bmp280/main/main.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include <bmp280.h>
+
+static const char *TAG = "temp_collector";
+
+#define SDA_GPIO 18
+#define SCL_GPIO 19
+
+static void measuring_task(void *pvParameters)
+{
+    bmp280_params_t params;
+    bmp280_init_default_params(&params);
+    bmp280_t dev;
+    memset(&dev, 0, sizeof(bmp280_t));
+
+    ESP_ERROR_CHECK(bmp280_init_desc(&dev, BMP280_I2C_ADDRESS_0, 0, SDA_GPIO, SCL_GPIO));
+    ESP_ERROR_CHECK(bmp280_init(&dev, &params));
+
+    bool bme280p = dev.id == BME280_CHIP_ID;
+    ESP_LOGI(TAG, "BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
+
+    float pressure, temperature, humidity;
+
+    while(1) {
+
+        if (bmp280_read_float(&dev, &temperature, &pressure, &humidity) != ESP_OK) {
+            ESP_LOGI(TAG, "Temperature/pressure reading failed\n");
+        } else {
+            ESP_LOGI(TAG, "Pressure: %.2f Pa, Temperature: %.2f C", pressure, temperature);
+        }    
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(i2cdev_init());
+
+    xTaskCreate(&measuring_task, "measuring_task", 4096, NULL, 5, NULL);
+}
