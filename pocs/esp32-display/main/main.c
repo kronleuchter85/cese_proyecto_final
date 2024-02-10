a599c0d3a6257f72ed43af9287d27d93d8ecda09
Gonzalo Carreno <kronleuchter85@gmail.com>
2024-02-10 13:16:31 +0000

    poc del display

--- pocs/esp32-display/main/main.c
+++ pocs/esp32-display/main/main.c
@@ -10,16 +10,17 @@
 #include "sdkconfig.h"
 #include "HD44780.h"
 
-#define LCD_ADDR 0x27
+#define DISPLAY_SDA_PIN  21
+#define DISPLAY_SCL_PIN  22
 
-#define SDA_PIN  21
-#define SCL_PIN  22
+#define DISPLAY_LCD_ADDR 0x27
+#define DISPLAY_LCD_COLS 16
+#define DISPLAY_LCD_ROWS 2
 
-#define LCD_COLS 16
-#define LCD_ROWS 2
+static void LCD_DemoTask(void* param){
 
-void LCD_DemoTask(void* param)
-{
+    LCD_init(DISPLAY_LCD_ADDR, DISPLAY_SDA_PIN, DISPLAY_SCL_PIN, DISPLAY_LCD_COLS, DISPLAY_LCD_ROWS);
+
     char num[20];
     while (true) {
         LCD_home();
@@ -27,22 +28,37 @@ void LCD_DemoTask(void* param)
         LCD_writeStr("16x2 I2C LCD");
         vTaskDelay(3000 / portTICK_RATE_MS);
         LCD_clearScreen();
+        LCD_setCursor(0,0);
         LCD_writeStr("Lets Count 0-10!");
         vTaskDelay(3000 / portTICK_RATE_MS);
         LCD_clearScreen();
         for (int i = 0; i <= 10; i++) {
-            LCD_setCursor(8, 2);
-            sprintf(num, "%d", i);
+
+            LCD_home();
+            LCD_setCursor(0, 0);
+            sprintf(num, "T: %.1f C", 23.5);
             LCD_writeStr(num);
+            
+            LCD_setCursor(8, 0);
+            sprintf(num, "H: %d %%", 30);
+            LCD_writeStr(num);
+            
+            LCD_setCursor(0, 1);
+            sprintf(num, "P: %.1f hP", 23.5);
+            LCD_writeStr(num);
+            
+            LCD_setCursor(8, 1);
+            sprintf(num, "L: %d %%", 30);
+            LCD_writeStr(num);
+            
             vTaskDelay(1000 / portTICK_RATE_MS);
         }
     }
 }
 
-void app_main(void)
-{
-    LCD_init(LCD_ADDR, SDA_PIN, SCL_PIN, LCD_COLS, LCD_ROWS);
-    xTaskCreate(&LCD_DemoTask, "Demo Task", 2048, NULL, 5, NULL);
+void app_main(void){
+    
+    xTaskCreate(LCD_DemoTask, "Demo Task", 2048, NULL, 5, NULL);
     
 }
 
